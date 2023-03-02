28f9c099fc5bb107c6b7bd80acf503aae36e07cf
Maksym Fedorenko <m.fedorenko@upswot.com>
2023-03-02 22:57:34 +0200

    [Added] Project with infrastructure and basic database context

---
+++ Kyoto.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,75 @@
+using Kyoto.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Kyoto.Infrastructure;
+
+public class DatabaseContext : DbContext, IDatabaseContext
+{
+    private string _connectionString;
+    
+    public DatabaseContext(string connectionString = "Host=;Port=;Database=;Username=;Password=;")
+    {
+        _connectionString = connectionString;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+    
+    public async Task MigrateAsync(string connectionString, CancellationToken cancellationToken = default)
+    {
+        _connectionString = connectionString;
+        await Database.MigrateAsync(cancellationToken);
+    }
+
+    public new EntityEntry<TEntity> Add<TEntity>(TEntity entity) where TEntity : BaseModel
+    {
+        return base.Add(entity);
+    }
+
+    public new ValueTask<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) where TEntity : BaseModel
+    {
+        return base.AddAsync(entity, cancellationToken);
+    }
+
+    public Task AddRangeAsync<TEntity>(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default) where TEntity : BaseModel
+    {
+        return base.AddRangeAsync(entities, cancellationToken);
+    }
+
+    public new DbSet<TEntity> Set<TEntity>() where TEntity : BaseModel
+    {
+        return base.Set<TEntity>();
+    }
+
+    public new void Update<TEntity>(TEntity entity) where TEntity : BaseModel
+    {
+        entity.ModificationTime = DateTime.UtcNow;
+        base.Update(entity);
+    }
+
+    public void UpdateRange<TEntity>(IEnumerable<TEntity> entities) where TEntity : BaseModel
+    {
+        foreach (TEntity entity in entities)
+        {
+            Update(entity);
+        }
+    }
+
+    public new void Remove<TEntity>(TEntity entity) where TEntity : BaseModel
+    {
+        base.Remove(entity);
+    }
+
+    public void RemoveRange<TEntity>(IEnumerable<TEntity> entities) where TEntity : BaseModel
+    {
+        base.RemoveRange(entities);
+    }
+
+    public void DetachAll()
+    {
+        ChangeTracker.Clear();
+    }
+}
---
+++ Kyoto.Infrastructure/Models/BaseModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Kyoto.Infrastructure.Models;
+
+public abstract class BaseModel
+{
+    private Guid _id;
+    [Key]
+    public virtual Guid Id {
+        get {
+            _id = _id == Guid.Empty ? Guid.NewGuid() : _id;
+            return _id;
+        }
+        set => _id = value;
+    }
+
+    public virtual DateTime? CreationTime { get; set; } = DateTime.UtcNow;
+    public virtual DateTime? ModificationTime { get; set; } = DateTime.UtcNow;
+}
